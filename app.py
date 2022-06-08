edc00ca99cf5500e8429225b1bfa5d267e632296
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-06-08 15:28:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,151 +1,163 @@
 # Core Pkgs
 import streamlit as st 
-import pandas as pd 
-import matplotlib.pyplot as plt 
-import matplotlib
-matplotlib.use('Agg')
-import seaborn as sns 
-import altair as alt
-from datetime import datetime
+import cv2
+from PIL import Image,ImageEnhance
+import numpy as np 
+import os
 
-# Online ML Pkgs
-from river.naive_bayes import MultinomialNB
-from river.feature_extraction import BagOfWords,TFIDF
-from river.compose import Pipeline
-
-# Training Data
-data = [("my unit test failed","software"),
-("tried the program, but it was buggy","software"),
-("i need a new power supply","hardware"),
-("the drive has a 2TB capacity","hardware"),
-("unit-tests","software"),
-("program","software"),
-("power supply","hardware"),
-("drive","hardware"),
-("it needs more memory","hardware"),
-("check the API","software"),
-("design the API","software"),
-("they need more CPU","hardware"),
-("code","software"),
-("i found some bugs in the code","software"),
-("i swapped the memory","hardware"),
-("i tested the code","software")]
+@st.cache
+def load_image(img):
+	im = Image.open(img)
+	return im
 
 
-# Model Building
-model = Pipeline(('vectorizer',BagOfWords(lowercase=True)),('nv',MultinomialNB()))
-for x,y in data:
-	model = model.learn_one(x,y)
+face_cascade = cv2.CascadeClassifier('frecog/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('frecog/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('frecog/haarcascade_smile.xml')
 
-# Storage in A Database
-import sqlite3
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
-
-# Create Fxn From SQL
-def create_table():
-	c.execute('CREATE TABLE IF NOT EXISTS predictionTable(message TEXT,prediction TEXT,probability NUMBER,software_proba NUMBER,hardware_proba NUMBER,postdate DATE)')
-
-
-def add_data(message,prediction,probability,software_proba,hardware_proba,postdate):
-    c.execute('INSERT INTO predictionTable(message,prediction,probability,software_proba,hardware_proba,postdate) VALUES (?,?,?,?,?,?)',(message,prediction,probability,software_proba,hardware_proba,postdate))
-    conn.commit()
-
-def view_all_data():
-	c.execute("SELECT * FROM predictionTable")
-	data = c.fetchall()
-	return data
+def detect_faces(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	# Detect faces
+	faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+	# Draw rectangle around the faces
+	for (x, y, w, h) in faces:
+				 cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+	return img,faces 
 
 
+def detect_eyes(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)
+	for (ex,ey,ew,eh) in eyes:
+	        cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+	return img
+
+def detect_smiles(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	# Detect Smiles
+	smiles = smile_cascade.detectMultiScale(gray, 1.1, 4)
+	# Draw rectangle around the Smiles
+	for (x, y, w, h) in smiles:
+	    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+	return img
+
+def cartonize_image(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	# Edges
+	gray = cv2.medianBlur(gray, 5)
+	edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+	#Color
+	color = cv2.bilateralFilter(img, 9, 300, 300)
+	#Cartoon
+	cartoon = cv2.bitwise_and(color, color, mask=edges)
+
+	return cartoon
+
+
+def cannize_image(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	img = cv2.GaussianBlur(img, (11, 11), 0)
+	canny = cv2.Canny(img, 100, 150)
+	return canny
 
 def main():
-	menu = ["Home","Manage","About"]
-	create_table()
+	"""Face Detection App"""
 
-	choice = st.sidebar.selectbox("Menu",menu)
-	if choice == "Home":
-		st.subheader("Home")
-		with st.form(key='mlform'):
-			col1,col2 = st.beta_columns([2,1])
-			with col1:
-				message = st.text_area("Message")
-				submit_message = st.form_submit_button(label='Predict')
+	st.title("Face Detection App")
+	st.text("Build with Streamlit and OpenCV")
 
-			with col2:
-				st.write("Online Incremental ML")
-				st.write("Predict Text as Software or Hardware Related")
+	activities = ["Detection","About"]
+	choice = st.sidebar.selectbox("Select Activty",activities)
 
-		if submit_message:
-			prediction = model.predict_one(message)
-			prediction_proba = model.predict_proba_one(message)	
-			probability = max(prediction_proba.values())
-			postdate = datetime.now()
-			# Add Data To Database
-			add_data(message,prediction,probability,prediction_proba['software'],prediction_proba['hardware'],postdate)
-			st.success("Data Submitted")
+	if choice == 'Detection':
+		st.subheader("Face Detection")
 
+		image_file = st.file_uploader("Upload Image",type=['jpg','png','jpeg'])
 
-			res_col1 ,res_col2 = st.beta_columns(2)
-			with res_col1:
-				st.info("Original Text")
-				st.write(message)
+		if image_file is not None:
+			our_image = Image.open(image_file)
+			st.text("Original Image")
+			# st.write(type(our_image))
+			st.image(our_image)
 
-				st.success("Prediction")
-				st.write(prediction)
+			enhance_type = st.sidebar.radio("Enhance Type",["Original","Gray-Scale","Contrast","Brightness","Blurring"])
+			if enhance_type == 'Gray-Scale':
+				new_img = np.array(our_image.convert('RGB'))
+				img = cv2.cvtColor(new_img,1)
+				gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+				# st.write(new_img)
+				st.image(gray)
+			elif enhance_type == 'Contrast':
+				c_rate = st.sidebar.slider("Contrast",0.5,3.5)
+				enhancer = ImageEnhance.Contrast(our_image)
+				img_output = enhancer.enhance(c_rate)
+				st.image(img_output)
 
-			with res_col2:
-				st.info("Probability")
-				st.write(prediction_proba)
+			elif enhance_type == 'Brightness':
+				c_rate = st.sidebar.slider("Brightness",0.5,3.5)
+				enhancer = ImageEnhance.Brightness(our_image)
+				img_output = enhancer.enhance(c_rate)
+				st.image(img_output)
 
-				# Plot of Probability
-				df_proba = pd.DataFrame({'label':prediction_proba.keys(),'probability':prediction_proba.values()})
-				# st.dataframe(df_proba)
-				# visualization
-				fig = alt.Chart(df_proba).mark_bar().encode(x='label',y='probability')
-				st.altair_chart(fig,use_container_width=True)	
-
-
-
-
-
-	elif choice == "Manage":
-		st.subheader("Manage & Monitor Results")
-		stored_data =  view_all_data() 
-		new_df = pd.DataFrame(stored_data,columns=['message','prediction','probability','software_proba','hardware_proba','postdate'])
-		st.dataframe(new_df)
-		new_df['postdate'] = pd.to_datetime(new_df['postdate'])
-
-		# c = alt.Chart(new_df).mark_line().encode(x='minutes(postdate)',y='probability')# For Minutes
-		c = alt.Chart(new_df).mark_line().encode(x='postdate',y='probability')
-		st.altair_chart(c)
-	
-		c_software_proba = alt.Chart(new_df['software_proba'].reset_index()).mark_line().encode(x='software_proba',y='index')
-		c_hardware_proba = alt.Chart(new_df['hardware_proba'].reset_index()).mark_line().encode(x='hardware_proba',y='index')
-		
-		
-
-		c1,c2 = st.beta_columns(2)
-		with c1:
-			with st.beta_expander("Software Probability"):
-				st.altair_chart(c_software_proba,use_container_width=True)
-
-		with c2:
-			with st.beta_expander("Hardware Probability"):
-				st.altair_chart(c_hardware_proba,use_container_width=True)
+			elif enhance_type == 'Blurring':
+				new_img = np.array(our_image.convert('RGB'))
+				blur_rate = st.sidebar.slider("Brightness",0.5,3.5)
+				img = cv2.cvtColor(new_img,1)
+				blur_img = cv2.GaussianBlur(img,(11,11),blur_rate)
+				st.image(blur_img)
+			elif enhance_type == 'Original':
+				
+				st.image(our_image,width=300)
+			else:
+				st.image(our_image,width=300)
 
 
-		with st.beta_expander("Prediction Distribution"):
-			fig2 = plt.figure()
-			sns.countplot(x='probability',data=new_df)
-			st.pyplot(fig2)
-		
+		# Face Detection
+		task = ["Faces","Smiles","Eyes","Cannize","Cartonize"]
+		feature_choice = st.sidebar.selectbox("Find Features",task)
+		if st.button("Process"):
+
+			if feature_choice == 'Faces':
+				result_img,result_faces = detect_faces(our_image)
+				st.image(result_img)
+
+				st.success("Found {} faces".format(len(result_faces)))
+			elif feature_choice == 'Smiles':
+				result_img = detect_smiles(our_image)
+				st.image(result_img)
 
 
-	else:
-		st.subheader("About")
+			elif feature_choice == 'Eyes':
+				result_img = detect_eyes(our_image)
+				st.image(result_img)
+
+			elif feature_choice == 'Cartonize':
+				result_img = cartonize_image(our_image)
+				st.image(result_img)
+
+			elif feature_choice == 'Cannize':
+				result_canny = cannize_image(our_image)
+				st.image(result_canny)
+
+
+
+
+	elif choice == 'About':
+		st.subheader("About Face Detection App")
+		st.markdown("Built with Streamlit by [JCharisTech](https://www.jcharistech.com/)")
+		st.text("Jesse E.Agbe(JCharis)")
+		st.success("Jesus Saves @JCharisTech")
 
 
 
 if __name__ == '__main__':
-	main()
-
+		main()	
